055db11733c8367073d0f4bef0752f69d955197e
hanendyo <hanendyo47@gmail.com>
2023-01-19 19:48:17 +0000

    feat: 2

--- number_guess.sh
+++ number_guess.sh
@@ -10,3 +10,14 @@ BEST_GAME_RESULT=$($PSQL "SELECT best_game FROM player WHERE LOWER(username)=LOW
 GAME_RESULT=$($PSQL "SELECT game_played, best_game FROM player WHERE LOWER(username)=LOWER('$USERNAME')")
 PLAYER_ID=$($PSQL "SELECT player_id FROM player WHERE LOWER(username)=LOWER('$USERNAME')")
 RANDOM_NUMBER=$(( RANDOM % 900 + 100))
+
+if [[ -z $USERNAME_RESULT ]]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USERNAME=$($PSQL "INSERT INTO player(username) VALUES('$USERNAME')")
+else
+  echo $GAME_RESULT | while IFS=' | ' read GAME_PLAYED BEST_GAME
+  do
+    echo "Welcome back, $USERNAME_RESULT! You have played $GAME_PLAYED_RESULT games, and your best game took $BEST_GAME_RESULT guesses."
+  done
+fi
